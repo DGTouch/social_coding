79079c8ebecfbf9a4d241f95b2dc657be18c11d5
todd <tmatthews0020@gmail.com>
2015-02-07 12:12:34 -0500

    users can sign in and out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
     else
-      # Create an error message.
+      flash.now[:danger] = 'Invalid email/password'
+      render 'new'
     end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
 private
